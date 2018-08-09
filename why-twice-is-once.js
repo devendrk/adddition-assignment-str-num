607d110adefeeca82294a50708e952784110d75a
Devendra <katwaldevendra@hotmail.com>
2018-08-09 15:53:03 +0300

    Create why-twice-is-once.js

---
+++ why-twice-is-once.js
@@ -0,0 +1,14 @@
+
+debugger;
+let input = 0;
+(input += String(input += 1)) % 6;
+leti input = 0;
+// input:(number,0)
+input += 1;
+// input:(number,1)
+string(input);
+// inp;u: (stirn,"1");
+input += "1";
+// input(stirng,"11");
+"11"%6;
+// (number,5)
